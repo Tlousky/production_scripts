e861996055fd53a002b9d54904e80e19afc70e69
Tamir Lousky <tlousky@gmail.com>
2013-02-19 15:53:48 +0200

    continuing to write the bone selection panel maker
    File now writes lines that will reselect all currently selected bones
    and deselect all other bones
    
    TODO:
    Try to deslect all bones with the operators, probably faster and easier.
    Less code.

--- bone_selection_panel_maker.py
+++ bone_selection_panel_maker.py
@@ -1,10 +1,23 @@
 import bpy, time
 
-prefix   = 'bone_selector_'
-now_time = time.localtime( time.time())
-filename = prefix + str(now_time)
+prefix   = 'bone_selector_'             # Textfile prefix
+now_time = time.localtime( time.time()) # Current time
+filename = prefix + str(now_time)       # Concatenate prefix and time to create unique name
 
-for bone in bpy.context.object.data.bones:
-     if bone.select == True:
-        print( "bpy.context.object.data.bones['%s'].select = True" % bone.name )
+# Create a new text file in the text editor
+bpy.data.texts.new(name=filename)
+textfile = bpy.data.texts[filename]
 
+start_string = """ import bpy
+
+"""
+
+# List all existing bone names
+bone_names     = [ name for bone.name in bpy.context.object.data.bones ]
+selected_bones = [ name for bone.name in bpy.context.object.data.bones if bone.select == True ]
+
+for name in bone_names:
+   if name in selected_bones:
+      textfile.write("bpy.context.object.data.bones['%s'].select = True" % bone.name  + "\n")
+   else:
+      textfile.write("bpy.context.object.data.bones['%s'].select = False" % bone.name  + "\n")

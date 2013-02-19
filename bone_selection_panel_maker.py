2d11953d85af4585568c5a1c862bb6581ed16c89
Tamir Lousky <tlousky@gmail.com>
2013-02-19 16:12:57 +0200

    Starting to add panel creation text

--- bone_selection_panel_maker.py
+++ bone_selection_panel_maker.py
@@ -8,7 +8,27 @@ filename = prefix + str(now_time)       # Concatenate prefix and time to create 
 bpy.data.texts.new(name=filename)
 textfile = bpy.data.texts[filename]
 
-start_string = """ import bpy
+start_string = """
+bl_info = {
+    "name"     : "Bone Selection Panel",
+    "category" : "Animation",
+    "author"   : "Tamir Lousky",
+    "version"  : "1.0"
+}
+
+import bpy
+
+class bone_selection_panel(bpy.types.Panel):
+    bl_idname      = "boneSelectionPanel"
+    bl_label       = "Bone Selection Panel"
+    bl_space_type  = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_context     = 'posemode'
+
+    def draw( self, context) :
+        layout = self.layout
+        layout.operator("pose.bone_selector_op")
+
 
 """
 
@@ -17,6 +37,8 @@ bone_names     = [ name for bone.name in bpy.context.object.data.bones ]
 selected_bones = [ name for bone.name in bpy.context.object.data.bones if bone.select == True ]
 
 for name in bone_names:
+   # TODO:
+   # Check if you can deslect all bones via bpy operators
    if name in selected_bones:
       textfile.write("bpy.context.object.data.bones['%s'].select = True" % bone.name  + "\n")
    else:
